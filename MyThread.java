08b7fb70155c686e7cc563e9a61f7452ecffc8f5
sahilawatramani <sahilawatramani@gmail.com>
2025-04-19 20:20:11 +0530

    Added runnable threads

---
+++ MyThread.java
@@ -0,0 +1,22 @@
+class MyThread implements Runnable {
+    protected int init;
+    protected int increment;
+    protected int delay;
+
+    public MyThread(int init, int increment, int delay) {
+        this.init = init;
+        this.increment = increment;
+        this.delay = delay;
+    }
+
+    public void run() {
+        try {
+            for (;;) {
+                System.out.println(init + " ");
+                init += increment;
+                Thread.sleep(delay);
+            }
+        } catch (InterruptedException e) {}
+    }
+}
+
